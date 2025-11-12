43b6eb8782b85342b25f18dbd8783c1e1afaee5c
tJuri-sub <juricuber@gmail.com>
2025-11-12 16:49:07 +0800

    Day2 cs: declare 2 string variables, 1 is hello, 1 is world, concatenate to say hello world

---
+++ cSharp/Day2.cs
@@ -0,0 +1,8 @@
+//declare 2 string variables, 1 is hello, 1 is world, concatenate to say hello world
+
+string str1 = "Hello";
+string str2 = "World";
+
+Console.WriteLine(string.Concat(str1, str2));
+
+Console.WriteLine(str1 + " " + str2);
