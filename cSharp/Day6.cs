bf72b0523677567fca3c842026f5c6ffb502601b
tJuri-sub <juricuber@gmail.com>
2025-11-16 13:21:04 +0800

    Day6 cs: concatenate integer and string without getting an error

---
+++ cSharp/Day6.cs
@@ -0,0 +1,9 @@
+//concatenate integer and string without getting an error
+
+int age = 21;
+
+string name = "Juri Turiano";
+
+string convertIntToString = age.ToString();
+
+Console.WriteLine("Age: " + convertIntToString + ' ' + "Name: " + name );
