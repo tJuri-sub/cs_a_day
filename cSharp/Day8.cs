5fbfdf53096e84f5498b5e51184537d2e1e6deb4
tJuri-sub <juricuber@gmail.com>
2025-11-18 21:47:38 +0800

    Day8 cs: contatenate boolean and string without getting an error

---
+++ cSharp/Day8.cs
@@ -0,0 +1,10 @@
+//contatenate boolean and string without getting an error
+
+bool isString = true;
+
+string name = "juri turiano";
+
+Console.WriteLine(string.Concat(isString, " ", name));
+
+Console.WriteLine(isString + " " + name);
+
