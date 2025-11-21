29ff7a630573402afbe676d9fae6ee0201e8c305
tJuri-sub <juricuber@gmail.com>
2025-11-21 19:08:15 +0800

    Day9 cs: Print the array without getting an error or index going out of range

---
+++ cSharp/Day9.cs
@@ -0,0 +1,9 @@
+//Print the array without getting an error or index going out of range
+
+
+int[] word = [1,2,3,4,5,6,7,8,9,10];
+
+for(int i = word[0]; i <= 9; i++)
+{
+    Console.WriteLine(word[i]);
+}
